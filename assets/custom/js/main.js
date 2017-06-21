c47caf9e0c2cb4303195ab5c44d6242bda73b3bb
TheLeafHouse <github@leafhouse.de>
2017-06-21 15:57:55 +0200

    Visibility from notification

--- assets/custom/js/main.js
+++ assets/custom/js/main.js
@@ -1,7 +1,12 @@
 Vue.component('playgroundnotification', {
   props: ['title'],
+  data() {
+    return {
+      isVisable: true
+    }
+  },
   template: `
-  <div class="notification is-info">
+  <div class="notification is-info" v-show="isVisable">
     <button class="delete"></button>
     <h3>{{title}}</h3>
     <p><slot></slot></p>
